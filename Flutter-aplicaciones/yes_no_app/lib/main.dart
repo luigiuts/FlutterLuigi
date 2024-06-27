3186afcd3caa0fd4f41a12042fe16a0ce1678589
Luigi Estevez <lsestevez@uts.edu.co>
2024-06-27 09:16:35 -0500

    6-27-2024 chat

---
+++ Flutter-aplicaciones/yes_no_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).theme(),
+      home: const ChatScreen()
+        // appBar: AppBar(
+        //   title: const Text('Material App Bar'),
+        // ),
+        // body:  Center(
+        //   child: FilledButton.tonal(
+        //     onPressed: (){ },
+        //     child: const Text('Click me')
+      //        ),
+      //   ),
+      // ),
+    );
+  }
+}
---
+++ Flutter-aplicaciones/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,54 @@
+import "package:flutter/material.dart";
+import "package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart";
+import "package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart";
+import "package:yes_no_app/presentation/widgets/shared/message_field_box.dart";
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://cnnespanol.cnn.com/wp-content/uploads/2024/06/messiintvjpv.jpg?quality=100&strip=info'),
+          ),
+        ),
+        title: const Text('Mi amor'),
+        centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (BuildContext context, int index) {
+                  return (index % 2 == 0)
+                  ? const HerMessageBubble()
+                  : const MyMessageBubble();
+                },
+              ),
+            ),
+            //caja de texto
+            MessageFieldBox(),
+            const Text('Hola')
+          ],
+        ),
+      ),
+    );
+  }
+}
